e2d957c54d1f3824abf9b303ab2281ee7edd62f6
castecabas <124809053+castecabas@users.noreply.github.com>
2025-02-07 16:52:48 -0500

    Delete .history directory

--- .history/Scripts/testing/test_20241124202516.py
+++
@@ -1,9 +0,0 @@
-
-from ultralytics import YOLO
-
-
-model = YOLO("C:/Users/castecabas/PycharmProjects/Recycler_Proyect/S_Training/runs/detect/train6/weights/best.pt")
-
-results= model.predict(source="0",show=True,imgsz=640,conf=0.5)
-
-print(results)
